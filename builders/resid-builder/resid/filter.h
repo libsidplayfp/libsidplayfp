6ecda5253df63dc3700ebc61081c3a5f17199841
Antti S. Lankila <alankila@users.sourceforge.net>
2009-04-06 16:34:28 +0000

    Can't put so much lpleak into bp; so invented a compromise...
    git-svn-id: https://svn.code.sf.net/p/sidplay-residfp/code/trunk/sidplay-libs-2.1.1@52 75748be7-0387-4326-94c7-f41ce0883d2c

--- builders/resid-builder/resid/filter.h
+++ builders/resid-builder/resid/filter.h
@@ -204,7 +204,7 @@ friend class SID;
 
 const float kinkiness = 0.966f;
 const float sidcaps_6581 = 470e-12f;
-const float outputleveldifference = 1.2f;
+const float outputleveldifference = 1.25f;
 
 inline
 static float fastexp(float val) {
@@ -322,6 +322,7 @@ float Filter::clock(float voice1,
     if (model == MOS6581) {
         /* output strip mixing to filter state */
         float lpleak = Vi * distortion_rate + Vhp + Vlp - Vbp * _1_div_Q;
+        lpleak *= 0.5f;
         if (hp_bp_lp & 2) {
             Vbp += (Vf - Vbp) * distortion_cf_threshold;
             Vf -= lpleak;
@@ -351,7 +352,7 @@ float Filter::clock(float voice1,
         Vbp += (lpleak - Vbp) * distortion_cf_threshold * _1_div_Q;
         Vhp += (lpleak - Vhp) * distortion_cf_threshold;
 
-	Vlp -= Vbp * type3_w0(Vbp - lpleak * 0.2f - type3_fc_distortion_offset) * outputleveldifference;
+	Vlp -= Vbp * type3_w0(Vbp - lpleak * 0.35f - type3_fc_distortion_offset) * outputleveldifference;
 	Vbp -= Vhp * type3_w0(Vhp - type3_fc_distortion_offset) * outputleveldifference;
 	Vhp = Vbp * _1_div_Q * (1.f/outputleveldifference)
             - Vlp * (1.f/outputleveldifference/outputleveldifference)
