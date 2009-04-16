5d51fd1bc2d8479d15db7ab8b84f9a3fb44385fd
Antti S. Lankila <alankila@users.sourceforge.net>
2009-04-16 23:33:56 +0000

    Tweaking never ends.
    git-svn-id: https://svn.code.sf.net/p/sidplay-residfp/code/trunk/sidplay-libs-2.1.1@70 75748be7-0387-4326-94c7-f41ce0883d2c

--- builders/resid-builder/resid/filter.h
+++ builders/resid-builder/resid/filter.h
@@ -324,7 +324,7 @@ float Filter::clock(float voice1,
     if (model == MOS6581) {
         /* output strip mixing to filter state */
         float lpleak = Vi * distortion_rate + Vhp + Vlp - Vbp * _1_div_Q;
-        lpleak *= 0.75f;
+        lpleak *= 0.7f;
         if (hp_bp_lp & 2) {
             Vbp += (Vf - Vbp) * distortion_cf_threshold;
             Vf -= lpleak;
@@ -354,7 +354,7 @@ float Filter::clock(float voice1,
         Vbp += (lpleak - Vbp) * distortion_cf_threshold * _1_div_Q;
         Vhp += (lpleak - Vhp) * distortion_cf_threshold;
 
-	Vlp -= Vbp * type3_w0(Vbp - lpleak * 0.17f - type3_fc_distortion_offset) * outputleveldifference;
+	Vlp -= Vbp * type3_w0(Vbp - lpleak * 0.2f - type3_fc_distortion_offset) * outputleveldifference;
 	Vbp -= Vhp * type3_w0(Vhp - type3_fc_distortion_offset) * outputleveldifference;
 	Vhp = Vbp * _1_div_Q * (1.f/outputleveldifference)
             - Vlp * (1.f/outputleveldifference/outputleveldifference)
