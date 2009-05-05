396f3914f62a358db9527e78a287744d08070f85
Antti S. Lankila <alankila@users.sourceforge.net>
2009-05-05 20:23:35 +0000

    Another dead end. Revert attempts to modify bp distortion based on lpleak.
    git-svn-id: https://svn.code.sf.net/p/sidplay-residfp/code/trunk/sidplay-libs-2.1.1@88 75748be7-0387-4326-94c7-f41ce0883d2c

--- builders/resid-builder/resid/filter.h
+++ builders/resid-builder/resid/filter.h
@@ -341,7 +341,7 @@ float Filter::clock(float voice1,
         Vbp += (lpleak - Vbp) * distortion_cf_threshold * _1_div_Q;
         Vhp += (lpleak - Vhp) * distortion_cf_threshold;
 
-	Vlp -= Vbp * type3_w0(Vbp - lpleak * 0.15f - type3_fc_distortion_offset) * outputleveldifference;
+	Vlp -= Vbp * type3_w0(Vbp - type3_fc_distortion_offset) * outputleveldifference;
 	Vbp -= Vhp * type3_w0(Vhp - type3_fc_distortion_offset) * outputleveldifference;
 	Vhp = Vbp * _1_div_Q * (1.f/outputleveldifference)
             - Vlp * (1.f/outputleveldifference/outputleveldifference)
