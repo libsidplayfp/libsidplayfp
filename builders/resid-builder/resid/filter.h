27c9d6eb5bbc99683cc2cd8c4d27993ad9453eb2
Antti S. Lankila <alankila@users.sourceforge.net>
2009-06-10 15:21:53 +0000

    Revert.
    git-svn-id: https://svn.code.sf.net/p/sidplay-residfp/code/trunk/sidplay-libs-2.1.1@123 75748be7-0387-4326-94c7-f41ce0883d2c

--- builders/resid-builder/resid/filter.h
+++ builders/resid-builder/resid/filter.h
@@ -206,7 +206,7 @@ friend class SID;
 // ----------------------------------------------------------------------------
 
 const float sidcaps_6581 = 470e-12f;
-const float outputleveldifference = 1.33f;
+const float outputleveldifference = 1.2f;
 
 inline
 static float fastexp(float val) {
@@ -312,6 +312,11 @@ float Filter::clock(float voice1,
     if (model == MOS6581) {
         /* output strip mixing to filter state */
         if (hp_bp_lp & 2) {
+            /* Some strange hybrid term in bp -- approximated as sum of all
+             * of filter's inputs and outputs in their respective levels. 
+             * Responsible for all manner of strange sounds in AMJ's music. */
+            float lpleak = (Vi * distortion_rate + Vhp + Vlp - Vbp * _1_div_Q);
+            Vf -= lpleak * 0.5f;
             Vbp += (Vf - Vbp) * distortion_cf_threshold;
         }
         if (hp_bp_lp & 1) {
@@ -326,15 +331,10 @@ float Filter::clock(float voice1,
             Vf -= (Vf - 3.3e6f) * 0.5f;
         }
             
-        /* Some strange hybrid term in bp -- approximated as sum of all
-         * of filter's inputs and outputs in their respective levels. 
-         * Responsible for all manner of strange sounds in AMJ's music. */
-        float lpleak = (Vi * distortion_rate + Vhp + Vlp - Vbp * _1_div_Q) * 0.33f;
-
-	Vlp -= (Vbp - lpleak) * type3_w0(Vbp - type3_fc_distortion_offset) * outputleveldifference;
-	Vbp -= Vhp * type3_w0(Vhp - type3_fc_distortion_offset);
-	Vhp = (Vbp + lpleak) * _1_div_Q
-               - Vlp * (1.f/outputleveldifference)
+	Vlp -= Vbp * type3_w0(Vbp - type3_fc_distortion_offset) * outputleveldifference;
+	Vbp -= Vhp * type3_w0(Vhp - type3_fc_distortion_offset) * outputleveldifference;
+	Vhp = Vbp * _1_div_Q * (1.f/outputleveldifference)
+               - Vlp * (1.f/outputleveldifference/outputleveldifference)
         /* the loss of level by about half is likely due to feedback
          * between Vhp amp input and output. */
             - Vi * distortion_rate;
