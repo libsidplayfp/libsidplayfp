3f1b9865a99ff11fe9d7c4a0f31144419e0a4625
Leandro Nini <drfiemost@users.sourceforge.net>
2011-07-31 14:00:39 +0000

    Add const
    git-svn-id: https://svn.code.sf.net/p/sidplay-residfp/code/trunk/libsidplayfp@312 75748be7-0387-4326-94c7-f41ce0883d2c

--- sidplayfp/c64/c64cia.h
+++ sidplayfp/c64/c64cia.h
@@ -41,7 +41,7 @@ protected:
 
     void portB ()
     {
-        uint8_t lp = (prb | ~ddrb) & 0x10;
+        const uint8_t lp = (prb | ~ddrb) & 0x10;
         if (lp != this->lp)
         {
             m_env.lightpen();
