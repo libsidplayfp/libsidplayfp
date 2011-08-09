6a1e1f677bff1f3cbf24fbbd0c4d9cb30df2e8b0
Leandro Nini <drfiemost@users.sourceforge.net>
2011-08-09 13:20:44 +0000

    Merge minor fixes from trunk
    git-svn-id: https://svn.code.sf.net/p/sidplay-residfp/code/branches/libsidplayfp-0.2@333 75748be7-0387-4326-94c7-f41ce0883d2c

--- sidplayfp/mos6526/mos6526.cpp
+++ sidplayfp/mos6526/mos6526.cpp
@@ -241,7 +241,8 @@ m_tbEvent.schedule(event_context, (event_clock_t) tb, m_phase);
 void MOS6526::tb_stop(void)
 {
 update_timers();
-if (m_tbEvent.pending()) m_tbEvent.cancel();
+if (m_tbEvent.pending())
+	m_tbEvent.cancel();
 }
 
 void MOS6526::tb_load(void)
@@ -375,7 +376,7 @@ switch (addr)
 			}
 		return data;}
 	case TAL:
-		return endian_16lo8(ta);
+		return endian_16lo8 (ta);
 	case TAH:
 		return endian_16hi8 (ta);
 	case TBL:
@@ -535,7 +536,7 @@ switch (addr)
 			break;
 			}
 		if (clearOneShot && tb == 1)
-			cra = data & 0xee;
+			crb = data & 0xee;
 		if ((crb & 0x61) == 0x01)
 			{   // Active
 			if (!m_tbEvent.pending())
