a5c56afa5d3a102823e436dd0150a4d27ef6b649
Leandro Nini <drfiemost@users.sourceforge.net>
2011-12-04 19:33:26 +0000

    Fix platform defines
    git-svn-id: https://svn.code.sf.net/p/sidplay-residfp/code/trunk/libsidplayfp@490 75748be7-0387-4326-94c7-f41ce0883d2c

--- sidplayfp/sidtune/SidTuneCfg.h
+++ sidplayfp/sidtune/SidTuneCfg.h
@@ -23,11 +23,11 @@
 /* Define the file/path separator(s) that your filesystem uses:
    SID_FS_IS_COLON_AND_BACKSLASH, SID_FS_IS_COLON_AND_SLASH,
    SID_FS_IS_BACKSLASH, SID_FS_IS_COLON, SID_FS_IS_SLASH  */
-#if defined(HAVE_MSWINDOWS) || defined(HAVE_MSDOS) || defined(HAVE_OS2)
+#if defined(_WIN32) || defined(_MSDOS) || defined(_OS2)
   #define SID_FS_IS_COLON_AND_BACKSLASH_AND_SLASH
-#elif defined(HAVE_MACOS)
+#elif defined(__APPLE__) && defined(__MACH__)
   #define SID_FS_IS_COLON
-#elif defined(HAVE_AMIGAOS)
+#elif defined(AMIGA)
   #define SID_FS_IS_COLON_AND_SLASH
 #else
   #define SID_FS_IS_SLASH
