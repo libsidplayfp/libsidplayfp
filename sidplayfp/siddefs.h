08bb920f43d196d35f1475f19399d3c901316853
Leandro Nini <drfiemost@users.sourceforge.net>
2012-10-13 10:00:35 +0000

    Update renamed file
    git-svn-id: https://svn.code.sf.net/p/sidplay-residfp/code/trunk/libsidplayfp@921 75748be7-0387-4326-94c7-f41ce0883d2c

--- sidplayfp/siddefs.h
+++ sidplayfp/siddefs.h
@@ -1,6 +1,27 @@
-/* sidconfig.h (template) */
-#ifndef SIDCONFIG_H
-#define SIDCONFIG_H
+/*
+ * This file is part of libsidplayfp, a SID player engine.
+ *
+ * Copyright 2011-2012 Leando Nini <drfiemost@users.sourceforge.net>
+ * Copyright 2007-2010 Antti Lankila
+ * Copyright 2000-2001 Simon White
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef SIDDEFS_H
+#define SIDDEFS_H
 
 /* DLL building support on win32 hosts */
 #ifndef SID_EXTERN
@@ -42,4 +63,4 @@
 #   define SIDPLAYFP_NAMESPACE_STOP
 #endif
 
-#endif /* SIDCONFIG_H */
+#endif /* SIDDEFS_H */
