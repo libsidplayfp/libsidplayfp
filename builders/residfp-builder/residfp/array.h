0b421c2f88da49b896985141f938d351a2a49f6d
Leandro Nini <drfiemost@users.sourceforge.net>
2014-09-06 18:39:07 +0000

    Rename class and remove buond checking
    git-svn-id: https://svn.code.sf.net/p/sidplay-residfp/code/trunk/libsidplayfp@1723 75748be7-0387-4326-94c7-f41ce0883d2c

--- builders/residfp-builder/residfp/array.h
+++ builders/residfp-builder/residfp/array.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of libsidplayfp, a SID player engine.
  *
- *  Copyright (C) 2011 Leandro Nini
+ *  Copyright (C) 2011-2014 Leandro Nini
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -22,6 +22,7 @@
 #define ARRAY_H
 
 /**
+ * Counter.
  */
 class counter
 {
@@ -35,38 +36,38 @@ public:
 };
 
 /**
- * Reference counted pointer to array wrapper, for use with standard containers.
+ * Reference counted pointer to matrix wrapper, for use with standard containers.
  */
 template<typename T>
-class array
+class matrix
 {
 private:
+    T* data;
     counter* count;
     const unsigned int x, y;
-    T* data;
 
 public:
-    array(unsigned int x, unsigned int y) :
+    matrix(unsigned int x, unsigned int y) :
+        data(new T[x * y]),
         count(new counter()),
         x(x),
-        y(y),
-        data(new T[x * y]) {}
+        y(y) {}
 
-    array(const array& p) :
+    matrix(const matrix& p) :
+        data(p.data),
         count(p.count),
         x(p.x),
-        y(p.y),
-        data(p.data) { count->increase(); }
+        y(p.y) { count->increase(); }
 
-    ~array() { if (count->decrease() == 0) { delete count; delete [] data; } }
+    ~matrix() { if (count->decrease() == 0) { delete count; delete [] data; } }
 
     unsigned int length() const { return x * y; }
 
-    T* operator[](unsigned int a) { return a < x ? &data[a * y] : 0; }
+    T* operator[](unsigned int a) { return &data[a * y]; }
 
-    T const* operator[](unsigned int a) const { return a < x ? &data[a * y] : 0; }
+    T const* operator[](unsigned int a) const { return &data[a * y]; }
 };
 
-typedef array<short> matrix_t;
+typedef matrix<short> matrix_t;
 
 #endif
