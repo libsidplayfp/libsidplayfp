afc060be8604e71389544704d630cb30f75dc882
Leandro Nini <drfiemost@users.sourceforge.net>
2014-10-28 17:19:55 +0000

    Use hex constant for consistency
    git-svn-id: https://svn.code.sf.net/p/sidplay-residfp/code/trunk/libsidplayfp@1824 75748be7-0387-4326-94c7-f41ce0883d2c

--- src/c64/Banks/SystemRAMBank.h
+++ src/c64/Banks/SystemRAMBank.h
@@ -50,7 +50,7 @@ public:
     void reset()
     {
         memset(ram, 0, 0x10000);
-        for (int i = 0x40; i < 0x10000; i += 128)
+        for (int i = 0x40; i < 0x10000; i += 0x80)
         {
             memset(ram+i, 0xff, 0x40);
         }
