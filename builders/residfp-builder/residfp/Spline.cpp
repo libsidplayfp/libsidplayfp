6c0b16db7cbb39a0b8f52d564255b00aab6e5a6e
Leandro Nini <drfiemost@users.sourceforge.net>
2015-05-03 15:03:14 +0000

    Merge r1935 from trunk
    git-svn-id: https://svn.code.sf.net/p/sidplay-residfp/code/branches/libsidplayfp-1.7@1936 75748be7-0387-4326-94c7-f41ce0883d2c

--- builders/residfp-builder/residfp/Spline.cpp
+++ builders/residfp-builder/residfp/Spline.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of libsidplayfp, a SID player engine.
  *
- * Copyright 2011-2014 Leandro Nini <drfiemost@users.sourceforge.net>
+ * Copyright 2011-2015 Leandro Nini <drfiemost@users.sourceforge.net>
  * Copyright 2007-2010 Antti Lankila
  *
  * This program is free software; you can redistribute it and/or modify
@@ -80,10 +80,6 @@ Spline::Spline(const Point input[], int inputLength) :
         params[i].b = (m - c1 - common) * invDx;
         params[i].a = common * invDx * invDx;
     }
-
-    // Fix the value ranges, because we interpolate outside original bounds if necessary.
-    params[0].x1 = std::numeric_limits<double>::min();
-    params[inputLength - 2].x2 = std::numeric_limits<double>::max();
 }
 
 Spline::Point Spline::evaluate(double x)
