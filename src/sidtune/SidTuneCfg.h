ffaa3f722c8d94fe63b08ab47aeeac4ac965c224
Leandro Nini <drfiemost@users.sourceforge.net>
2015-08-28 17:29:19 +0000

    Fix indentation and comments
    git-svn-id: https://svn.code.sf.net/p/sidplay-residfp/code/trunk/libsidplayfp@2026 75748be7-0387-4326-94c7-f41ce0883d2c

--- src/sidtune/SidTuneCfg.h
+++ src/sidtune/SidTuneCfg.h
@@ -7,29 +7,28 @@
 #  include "config.h"
 #endif
 
-/* Minimum load address for real c64 only tunes */
+// Minimum load address for real c64 only tunes
 #define SIDTUNE_R64_MIN_LOAD_ADDR 0x07e8
 
 
-/* --------------------------------------------------------------------------
- * Don't touch these!
- * --------------------------------------------------------------------------
- */
+// --------------------------------------------------------------------------
+// Don't touch these!
+// --------------------------------------------------------------------------
 #undef SIDTUNE_NO_STDIN_LOADER
 #undef SIDTUNE_REJECT_UNKNOWN_FIELDS
 
 
-/* Define the file/path separator(s) that your filesystem uses:
-   SID_FS_IS_COLON_AND_BACKSLASH, SID_FS_IS_COLON_AND_SLASH,
-   SID_FS_IS_BACKSLASH, SID_FS_IS_COLON, SID_FS_IS_SLASH  */
+// Define the file/path separator(s) that your filesystem uses:
+// SID_FS_IS_COLON_AND_BACKSLASH, SID_FS_IS_COLON_AND_SLASH,
+// SID_FS_IS_BACKSLASH, SID_FS_IS_COLON, SID_FS_IS_SLASH
 #if defined(_WIN32) || defined(_MSDOS) || defined(_OS2)
-  #define SID_FS_IS_COLON_AND_BACKSLASH_AND_SLASH
+#  define SID_FS_IS_COLON_AND_BACKSLASH_AND_SLASH
 #elif defined(__APPLE__) && defined(__MACH__)
-  #define SID_FS_IS_COLON
+#  define SID_FS_IS_COLON
 #elif defined(AMIGA)
-  #define SID_FS_IS_COLON_AND_SLASH
+#  define SID_FS_IS_COLON_AND_SLASH
 #else
-  #define SID_FS_IS_SLASH
+#  define SID_FS_IS_SLASH
 #endif
 
 #endif  /* SIDTUNECFG_H */
