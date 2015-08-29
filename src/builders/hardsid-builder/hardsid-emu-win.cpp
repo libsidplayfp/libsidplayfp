c575773abd7024caa780b28314d5a1225a5ad4a6
Leandro Nini <drfiemost@users.sourceforge.net>
2015-08-29 20:07:55 +0000

    Use nullptr
    git-svn-id: https://svn.code.sf.net/p/sidplay-residfp/code/trunk/libsidplayfp@2058 75748be7-0387-4326-94c7-f41ce0883d2c

--- src/builders/hardsid-builder/hardsid-emu-win.cpp
+++ src/builders/hardsid-builder/hardsid-emu-win.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of libsidplayfp, a SID player engine.
  *
- * Copyright 2011-2014 Leandro Nini <drfiemost@users.sourceforge.net>
+ * Copyright 2011-2015 Leandro Nini <drfiemost@users.sourceforge.net>
  * Copyright 2007-2010 Antti Lankila
  * Copyright 2000-2002 Simon White
  *
@@ -129,7 +129,7 @@ void HardSID::reset(uint8_t volume)
         hsid2.Reset((BYTE) m_instance);
     hsid2.Sync((BYTE) m_instance);
 
-    if (eventScheduler != 0)
+    if (eventScheduler != nullptr)
         eventScheduler->schedule(*this, HARDSID_DELAY_CYCLES, EVENT_CLOCK_PHI1);
 }
 
