1b7af52a426bffdb783d3efbf683326afacfb296
Leandro Nini <drfiemost@users.sourceforge.net>
2015-09-05 11:32:45 +0000

    Fix compiling with debug enabled
    git-svn-id: https://svn.code.sf.net/p/sidplay-residfp/code/trunk/libsidplayfp@2085 75748be7-0387-4326-94c7-f41ce0883d2c

--- src/c64/CPU/mos6510.cpp
+++ src/c64/CPU/mos6510.cpp
@@ -241,7 +241,7 @@ void MOS6510::interruptsAndNextOpcode()
 #ifdef DEBUG
         if (dodump)
         {
-            const event_clock_t cycles = eventContext.getTime(EVENT_CLOCK_PHI2);
+            const event_clock_t cycles = eventScheduler.getTime(EVENT_CLOCK_PHI2);
             fprintf(m_fdbg, "****************************************************\n");
             fprintf(m_fdbg, " interrupt (%d)\n", static_cast<int>(cycles));
             fprintf(m_fdbg, "****************************************************\n");
@@ -264,7 +264,7 @@ void MOS6510::fetchNextOpcode()
 #ifdef DEBUG
     if (dodump)
     {
-        MOS6510Debug::DumpState(eventContext.getTime(EVENT_CLOCK_PHI2), *this);
+        MOS6510Debug::DumpState(eventScheduler.getTime(EVENT_CLOCK_PHI2), *this);
     }
 
     instrStartPC = Register_ProgramCounter;
