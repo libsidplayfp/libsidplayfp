44cb6220be2527d5937918a57820680dcef3b3b5
Leandro Nini <drfiemost@users.sourceforge.net>
2016-05-02 20:00:51 +0000

    Minor refactoring
    git-svn-id: https://svn.code.sf.net/p/sidplay-residfp/code/trunk/libsidplayfp@2324 75748be7-0387-4326-94c7-f41ce0883d2c

--- src/builders/residfp-builder/residfp/ExternalFilter.cpp
+++ src/builders/residfp-builder/residfp/ExternalFilter.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of libsidplayfp, a SID player engine.
  *
- * Copyright 2011-2013 Leandro Nini <drfiemost@users.sourceforge.net>
+ * Copyright 2011-2016 Leandro Nini <drfiemost@users.sourceforge.net>
  * Copyright 2007-2010 Antti Lankila
  * Copyright 2004 Dag Lem <resid@nimrod.no>
  *
@@ -27,6 +27,17 @@
 namespace reSIDfp
 {
 
+/**
+ * Get the 3 dB attenuation point.
+ *
+ * @param res the resistance value in Ohms
+ * @param cap the capacitance value in Farads
+ */
+inline double getW0(double res, double cap)
+{
+    return 1. / (res * cap);
+}
+
 ExternalFilter::ExternalFilter() :
     Vlp(0),
     Vhp(0),
@@ -39,10 +50,10 @@ ExternalFilter::ExternalFilter() :
 void ExternalFilter::setClockFrequency(double frequency)
 {
     // Low-pass: R = 10kOhm, C = 1000pF; w0l = 1/RC = 1/(1e4*1e-9) = 100000
-    w0lp_1_s7 = static_cast<int>(100000. / frequency * (1 << 7) + 0.5);
+    w0lp_1_s7 = static_cast<int>(getW0(10e3, 1000e-12) / frequency * (1 << 7) + 0.5);
 
     // High-pass: R = 1kOhm, C = 10uF; w0h = 1/RC = 1/(1e3*1e-5) = 100
-    w0hp_1_s17 = static_cast<int>(100. / frequency * (1 << 17) + 0.5);
+    w0hp_1_s17 = static_cast<int>(getW0(1e3, 10e-6) / frequency * (1 << 17) + 0.5);
 }
 
 void ExternalFilter::reset()
