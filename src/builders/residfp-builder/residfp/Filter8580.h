9d717224347bb18679e33cb981e1d93bb7d58b21
Leandro Nini <drfiemost@users.sourceforge.net>
2020-04-09 13:18:47 +0000

    Typo
    git-svn-id: https://svn.code.sf.net/p/sidplay-residfp/code/trunk/libsidplayfp@2656 75748be7-0387-4326-94c7-f41ce0883d2c

--- src/builders/residfp-builder/residfp/Filter8580.h
+++ src/builders/residfp-builder/residfp/Filter8580.h
@@ -249,7 +249,7 @@ class Integrator8580;
  *
  *
  *
- * Crystal stabilized precision swithced capacitor voltage divider
+ * Crystal stabilized precision switched capacitor voltage divider
  * ---------------------------------------------------------------
  * There is a FET working as a temperature sensor close to the DACs which changes the gate voltage
  * of the frequency control DACs according to the temperature of the DACs,
