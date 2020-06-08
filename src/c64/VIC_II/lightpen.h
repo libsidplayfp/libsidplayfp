63878c8e59bd179e2821e74ad51084e382145c03
Leandro Nini <drfiemost@users.sourceforge.net>
2020-06-08 14:41:59 +0000

    Refactor and minor fix
    git-svn-id: https://svn.code.sf.net/p/sidplay-residfp/code/trunk/libsidplayfp@2698 75748be7-0387-4326-94c7-f41ce0883d2c

--- src/c64/VIC_II/lightpen.h
+++ src/c64/VIC_II/lightpen.h
@@ -131,22 +131,20 @@ public:
      */
     bool trigger(unsigned int lineCycle, unsigned int rasterY)
     {
-        if (!isTriggered)
+        if (isTriggered)
+            return false;
+
+        isTriggered = true;
+
+        // don't latch on the last line, except on the first cycle
+        if ((rasterY != lastLine) || (lineCycle == 0))
         {
-            // don't trigger on the last line, except on the first cycle
-            if ((rasterY == lastLine) && (lineCycle > 0))
-            {
-                return false;
-            }
-
-            isTriggered = true;
-
             // Latch current coordinates
             lpx = getXpos(lineCycle) + 2;
             lpy = rasterY;
-            return true;
         }
-        return false;
+
+        return true;
     }
 
     /**
