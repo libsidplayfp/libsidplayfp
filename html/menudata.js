cbeff68b0666ba7babe23b81e6e964a67553fc67
drfiemost <drfiemost@users.noreply.github.com>
2021-09-16 13:10:15 +0000

    Update docs dca1ff4ddf00812bf8ba184bef3465a740a75bf7

--- html/menudata.js
+++ html/menudata.js
@@ -73,6 +73,7 @@ var menudata={children:[
 {text:"w",url:"functions_func_w.html#index_w"},
 {text:"~",url:"functions_func_~.html#index__7E"}]},
 {text:"Variables",url:"functions_vars.html",children:[
+{text:"a",url:"functions_vars.html#index_a"},
 {text:"b",url:"functions_vars.html#index_b"},
 {text:"c",url:"functions_vars.html#index_c"},
 {text:"d",url:"functions_vars.html#index_d"},
