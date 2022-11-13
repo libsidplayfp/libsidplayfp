libsidplayfp/libsidplayfp

commit fafbfb087a09d1555d3a9db814e0b1e7ba3bdfd7

Do not use smart pointers in public headers [bugs:#38]
git-svn-id: https://svn.code.sf.net/p/sidplay-residfp/code/trunk/libsidplayfp@2821 75748be7-0387-4326-94c7-f41ce0883d2c

src/utils/SidDatabase.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of libsidplayfp, a SID player engine.
  *
- * Copyright 2011-2019 Leandro Nini <drfiemost@users.sourceforge.net>
+ * Copyright 2011-2020 Leandro Nini <drfiemost@users.sourceforge.net>
  * Copyright 2007-2010 Antti Lankila
  * Copyright 2000-2001 Simon White
  *
@@ -97,7 +97,8 @@ const char *parseTime(const char *str, int_least32_t &result)
 
 bool SidDatabase::open(const char *filename)
 {
-    m_parser.reset(new libsidplayfp::iniParser());
+    delete m_parser;
+    m_parser = new libsidplayfp::iniParser();
 
     if (!m_parser->open(filename))
     {
@@ -111,7 +112,8 @@ bool SidDatabase::open(const char *filename)
 
 void SidDatabase::close()
 {
-    m_parser.reset(nullptr);
+    delete m_parser;
+    m_parser = nullptr;
 }
 
 int_least32_t SidDatabase::length(SidTune &tune)
@@ -151,7 +153,7 @@ int_least32_t SidDatabase::length(const char *md5, unsigned int song)
 
 int_least32_t SidDatabase::lengthMs(const char *md5, unsigned int song)
 {
-    if (m_parser.get() == nullptr)
+    if (m_parser == nullptr)
     {
         errorString = ERR_NO_DATABASE_LOADED;
         return -1;

src/sidplayfp/SidTune.cpp
@@ -1,7 +1,7 @@
  /*
  * This file is part of libsidplayfp, a SID player engine.
  *
- * Copyright 2012-2015 Leandro Nini <drfiemost@users.sourceforge.net>
+ * Copyright 2012-2020 Leandro Nini <drfiemost@users.sourceforge.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -62,7 +62,7 @@ SidTune::SidTune(const uint_least8_t* oneFileFormatSidtune, uint_least32_t sidtu
 
 SidTune::~SidTune()
 {
-    // Needed to delete auto_ptr with complete type
+    delete tune;
 }
 
 void SidTune::setFileNameExtensions(const char **fileNameExt)
@@ -74,12 +74,14 @@ void SidTune::load(const char* fileName, bool separatorIsSlash)
 {
     try
     {
-        tune.reset(SidTuneBase::load(fileName, fileNameExtensions, separatorIsSlash));
+        delete tune;
+        tune = SidTuneBase::load(fileName, fileNameExtensions, separatorIsSlash);
         m_status = true;
         m_statusString = MSG_NO_ERRORS;
     }
     catch (loadError const &e)
     {
+        tune =  nullptr;
         m_status = false;
         m_statusString = e.message();
     }
@@ -89,12 +91,14 @@ void SidTune::read(const uint_least8_t* sourceBuffer, uint_least32_t bufferLen)
 {
     try
     {
-        tune.reset(SidTuneBase::read(sourceBuffer, bufferLen));
+        delete tune;
+        tune = SidTuneBase::read(sourceBuffer, bufferLen);
         m_status = true;
         m_statusString = MSG_NO_ERRORS;
     }
     catch (loadError const &e)
     {
+        tune =  nullptr;
         m_status = false;
         m_statusString = e.message();
     }
@@ -102,17 +106,17 @@ void SidTune::read(const uint_least8_t* sourceBuffer, uint_least32_t bufferLen)
 
 unsigned int SidTune::selectSong(unsigned int songNum)
 {
-    return tune.get() != nullptr ? tune->selectSong(songNum) : 0;
+    return tune != nullptr ? tune->selectSong(songNum) : 0;
 }
 
 const SidTuneInfo* SidTune::getInfo() const
 {
-    return tune.get() != nullptr ? tune->getInfo() : nullptr;
+    return tune != nullptr ? tune->getInfo() : nullptr;
 }
 
 const SidTuneInfo* SidTune::getInfo(unsigned int songNum)
 {
-    return tune.get() != nullptr ? tune->getInfo(songNum) : nullptr;
+    return tune != nullptr ? tune->getInfo(songNum) : nullptr;
 }
 
 bool SidTune::getStatus() const { return m_status; }
@@ -121,7 +125,7 @@ const char* SidTune::statusString() const { return m_statusString; }
 
 bool SidTune::placeSidTuneInC64mem(sidmemory& mem)
 {
-    if (tune.get() == nullptr)
+    if (tune == nullptr)
         return false;
 
     tune->placeSidTuneInC64mem(mem);
@@ -130,15 +134,15 @@ bool SidTune::placeSidTuneInC64mem(sidmemory& mem)
 
 const char* SidTune::createMD5(char *md5)
 {
-    return tune.get() != nullptr ? tune->createMD5(md5) : nullptr;
+    return tune != nullptr ? tune->createMD5(md5) : nullptr;
 }
 
 const char* SidTune::createMD5New(char *md5)
 {
-    return tune.get() != nullptr ? tune->createMD5New(md5) : nullptr;
+    return tune != nullptr ? tune->createMD5New(md5) : nullptr;
 }
 
 const uint_least8_t* SidTune::c64Data() const
 {
-    return tune.get() != nullptr ? tune->c64Data() : nullptr;
+    return tune != nullptr ? tune->c64Data() : nullptr;
 }

src/sidplayfp/SidTune.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of libsidplayfp, a SID player engine.
  *
- * Copyright 2011-2015 Leandro Nini <drfiemost@users.sourceforge.net>
+ * Copyright 2011-2020 Leandro Nini <drfiemost@users.sourceforge.net>
  * Copyright 2007-2010 Antti Lankila
  * Copyright 2000 Simon White
  *
@@ -24,7 +24,6 @@
 #define SIDTUNE_H
 
 #include <stdint.h>
-#include <memory>
 
 #include "sidplayfp/siddefs.h"
 
@@ -49,7 +48,7 @@ private:
     static const char** fileNameExtensions;
 
 private:  // -------------------------------------------------------------
-    std::auto_ptr<libsidplayfp::SidTuneBase> tune;
+    libsidplayfp::SidTuneBase* tune;
 
     const char* m_statusString;
 

src/utils/SidDatabase.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of libsidplayfp, a SID player engine.
  *
- * Copyright 2011-2019 Leandro Nini <drfiemost@users.sourceforge.net>
+ * Copyright 2011-2020 Leandro Nini <drfiemost@users.sourceforge.net>
  * Copyright 2007-2010 Antti Lankila
  * Copyright 2000-2001 Simon White
  *
@@ -24,7 +24,6 @@
 #define SIDDATABASE_H
 
 #include <stdint.h>
-#include <memory>
 
 #include "sidplayfp/siddefs.h"
 
@@ -42,7 +41,7 @@ class iniParser;
 class SID_EXTERN SidDatabase
 {
 private:
-    std::auto_ptr<libsidplayfp::iniParser> m_parser;
+    libsidplayfp::iniParser* m_parser;
 
     const char *errorString;
 

commit 95d5008b7d738cc94220e9ce7a9c903987ba1f3e

Fix mem leak introduced with fafbfb0

src/utils/SidDatabase.cpp
@@ -46,7 +46,7 @@ SidDatabase::SidDatabase() :
 
 SidDatabase::~SidDatabase()
 {
-    // Needed to delete auto_ptr with complete type
+    delete m_parser;
 }
 
 // mm:ss[.SSS]