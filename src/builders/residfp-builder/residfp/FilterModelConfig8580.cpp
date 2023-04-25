b5794186a9719f6a826eaa2bae7f58ead516c584
Leandro Nini <drfiemost@users.noreply.github.com>
2023-04-25 20:07:02 +0200

    Use OpenMP to speedup LUTs initialization

--- src/builders/residfp-builder/residfp/FilterModelConfig8580.cpp
+++ src/builders/residfp-builder/residfp/FilterModelConfig8580.cpp
@@ -25,6 +25,7 @@
 #include "Integrator8580.h"
 #include "OpAmp.h"
 
+
 namespace reSIDfp
 {
 
@@ -132,6 +133,7 @@ FilterModelConfig8580::FilterModelConfig8580() :
     )
 {
     // Create lookup tables for gains / summers.
+#ifndef _OPENMP
     OpAmp opampModel(
         std::vector<Spline::Point>(
             std::begin(opamp_voltage),
@@ -139,83 +141,135 @@ FilterModelConfig8580::FilterModelConfig8580() :
         Vddt,
         vmin,
         vmax);
+#endif
 
-    // The filter summer operates at n ~ 1, and has 5 fundamentally different
-    // input configurations (2 - 6 input "resistors").
-    //
-    // Note that all "on" transistors are modeled as one. This is not
-    // entirely accurate, since the input for each transistor is different,
-    // and transistors are not linear components. However modeling all
-    // transistors separately would be extremely costly.
-    for (int i = 0; i < 5; i++)
+    #pragma omp parallel sections
     {
-        const int idiv = 2 + i;        // 2 - 6 input "resistors".
-        const int size = idiv << 16;
-        const double n = idiv;
-        opampModel.reset();
-        summer[i] = new unsigned short[size];
-
-        for (int vi = 0; vi < size; vi++)
+        #pragma omp section
         {
-            const double vin = vmin + vi / N16 / idiv; /* vmin .. vmax */
-            summer[i][vi] = getNormalizedValue(opampModel.solve(n, vin));
+#ifdef _OPENMP
+            OpAmp opampModel(
+                std::vector<Spline::Point>(
+                    std::begin(opamp_voltage),
+                    std::end(opamp_voltage)),
+                Vddt,
+                vmin,
+                vmax);
+#endif
+            // The filter summer operates at n ~ 1, and has 5 fundamentally different
+            // input configurations (2 - 6 input "resistors").
+            //
+            // Note that all "on" transistors are modeled as one. This is not
+            // entirely accurate, since the input for each transistor is different,
+            // and transistors are not linear components. However modeling all
+            // transistors separately would be extremely costly.
+            for (int i = 0; i < 5; i++)
+            {
+                const int idiv = 2 + i;        // 2 - 6 input "resistors".
+                const int size = idiv << 16;
+                const double n = idiv;
+                opampModel.reset();
+                summer[i] = new unsigned short[size];
+
+                for (int vi = 0; vi < size; vi++)
+                {
+                    const double vin = vmin + vi / N16 / idiv; /* vmin .. vmax */
+                    summer[i][vi] = getNormalizedValue(opampModel.solve(n, vin));
+                }
+            }
         }
-    }
 
-    // The audio mixer operates at n ~ 8/5, and has 8 fundamentally different
-    // input configurations (0 - 7 input "resistors").
-    //
-    // All "on", transistors are modeled as one - see comments above for
-    // the filter summer.
-    for (int i = 0; i < 8; i++)
-    {
-        const int idiv = (i == 0) ? 1 : i;
-        const int size = (i == 0) ? 1 : i << 16;
-        const double n = i * 8.0 / 5.0;
-        opampModel.reset();
-        mixer[i] = new unsigned short[size];
-
-        for (int vi = 0; vi < size; vi++)
+        #pragma omp section
         {
-            const double vin = vmin + vi / N16 / idiv; /* vmin .. vmax */
-            mixer[i][vi] = getNormalizedValue(opampModel.solve(n, vin));
+#ifdef _OPENMP
+            OpAmp opampModel(
+                std::vector<Spline::Point>(
+                    std::begin(opamp_voltage),
+                    std::end(opamp_voltage)),
+                Vddt,
+                vmin,
+                vmax);
+#endif
+            // The audio mixer operates at n ~ 8/5, and has 8 fundamentally different
+            // input configurations (0 - 7 input "resistors").
+            //
+            // All "on", transistors are modeled as one - see comments above for
+            // the filter summer.
+            for (int i = 0; i < 8; i++)
+            {
+                const int idiv = (i == 0) ? 1 : i;
+                const int size = (i == 0) ? 1 : i << 16;
+                const double n = i * 8.0 / 5.0;
+                opampModel.reset();
+                mixer[i] = new unsigned short[size];
+
+                for (int vi = 0; vi < size; vi++)
+                {
+                    const double vin = vmin + vi / N16 / idiv; /* vmin .. vmax */
+                    mixer[i][vi] = getNormalizedValue(opampModel.solve(n, vin));
+                }
+            }
         }
-    }
 
-    // 4 bit "resistor" ladders in the audio output gain
-    // necessitate 16 gain tables.
-    // From die photographs of the volume "resistor" ladders
-    // it follows that gain ~ vol/16 (assuming ideal
-    // op-amps and ideal "resistors").
-    for (int n8 = 0; n8 < 16; n8++)
-    {
-        const int size = 1 << 16;
-        const double n = n8 / 16.0;
-        opampModel.reset();
-        gain_vol[n8] = new unsigned short[size];
-
-        for (int vi = 0; vi < size; vi++)
+        #pragma omp section
         {
-            const double vin = vmin + vi / N16; /* vmin .. vmax */
-            gain_vol[n8][vi] = getNormalizedValue(opampModel.solve(n, vin));
+#ifdef _OPENMP
+            OpAmp opampModel(
+                std::vector<Spline::Point>(
+                    std::begin(opamp_voltage),
+                    std::end(opamp_voltage)),
+                Vddt,
+                vmin,
+                vmax);
+#endif
+            // 4 bit "resistor" ladders in the audio output gain
+            // necessitate 16 gain tables.
+            // From die photographs of the volume "resistor" ladders
+            // it follows that gain ~ vol/16 (assuming ideal
+            // op-amps and ideal "resistors").
+            for (int n8 = 0; n8 < 16; n8++)
+            {
+                const int size = 1 << 16;
+                const double n = n8 / 16.0;
+                opampModel.reset();
+                gain_vol[n8] = new unsigned short[size];
+
+                for (int vi = 0; vi < size; vi++)
+                {
+                    const double vin = vmin + vi / N16; /* vmin .. vmax */
+                    gain_vol[n8][vi] = getNormalizedValue(opampModel.solve(n, vin));
+                }
+            }
         }
-    }
 
-    // 4 bit "resistor" ladders in the bandpass resonance gain
-    // necessitate 16 gain tables.
-    // From die photographs of the bandpass "resistor" ladders
-    // it follows that 1/Q ~ 2^((4 - res)/8) (assuming ideal
-    // op-amps and ideal "resistors").
-    for (int n8 = 0; n8 < 16; n8++)
-    {
-        const int size = 1 << 16;
-        opampModel.reset();
-        gain_res[n8] = new unsigned short[size];
-
-        for (int vi = 0; vi < size; vi++)
+        #pragma omp section
         {
-            const double vin = vmin + vi / N16; /* vmin .. vmax */
-            gain_res[n8][vi] = getNormalizedValue(opampModel.solve(resGain[n8], vin));
+#ifdef _OPENMP
+            OpAmp opampModel(
+                std::vector<Spline::Point>(
+                    std::begin(opamp_voltage),
+                    std::end(opamp_voltage)),
+                Vddt,
+                vmin,
+                vmax);
+#endif
+            // 4 bit "resistor" ladders in the bandpass resonance gain
+            // necessitate 16 gain tables.
+            // From die photographs of the bandpass "resistor" ladders
+            // it follows that 1/Q ~ 2^((4 - res)/8) (assuming ideal
+            // op-amps and ideal "resistors").
+            for (int n8 = 0; n8 < 16; n8++)
+            {
+                const int size = 1 << 16;
+                opampModel.reset();
+                gain_res[n8] = new unsigned short[size];
+
+                for (int vi = 0; vi < size; vi++)
+                {
+                    const double vin = vmin + vi / N16; /* vmin .. vmax */
+                    gain_res[n8][vi] = getNormalizedValue(opampModel.solve(resGain[n8], vin));
+                }
+            }
         }
     }
 }
