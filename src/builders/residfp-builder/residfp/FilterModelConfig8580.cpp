eb12978469eabd8e7b14a69c038359c8dab155b0
Leandro Nini <drfiemost@users.noreply.github.com>
2023-12-31 10:54:38 +0100

    Adjust 8580 voice voltage range to avoid cracks and pops

--- src/builders/residfp-builder/residfp/FilterModelConfig8580.cpp
+++ src/builders/residfp-builder/residfp/FilterModelConfig8580.cpp
@@ -135,7 +135,7 @@ FilterModelConfig8580* FilterModelConfig8580::getInstance()
 
 FilterModelConfig8580::FilterModelConfig8580() :
     FilterModelConfig(
-        0.30,   // voice voltage range FIXME measure
+        0.24,   // voice voltage range FIXME measure
         4.84,   // voice DC voltage FIXME measure
         22e-9,  // capacitor value
         9.09,   // Vdd
