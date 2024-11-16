4edfab2ffbf5014df419f4646aa321633e69199b
Acro <47704566+acrouzet@users.noreply.github.com>
2024-11-16 02:45:31 -0500

    Set msb_rising to 0 when a saw-combined waveform is selected. This prevents unwanted sync from occuring. (#169)

--- src/builders/residfp-builder/residfp/WaveformGenerator.h
+++ src/builders/residfp-builder/residfp/WaveformGenerator.h
@@ -368,14 +368,15 @@ unsigned int WaveformGenerator::output(const WaveformGenerator* ringModulator)
         {
             osc3 = waveform_output;
         }
-
+        
         // In the 6581 the top bit of the accumulator may be driven low by combined waveforms
         // when the sawtooth is selected
-        if (is6581
-                && (waveform & 0x2)
-                && ((waveform_output & 0x800) == 0))
+        if (is6581 && (waveform & 0x2) && ((waveform_output & 0x800) == 0))
+        {
+            msb_rising = 0;
             accumulator &= 0x7fffff;
-
+        }
+        
         write_shift_register();
     }
     else
