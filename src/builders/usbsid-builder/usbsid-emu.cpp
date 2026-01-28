libsidplayfp/libsidplayfp

commit 178d7786b8f26fa03e6ad17c3e77cf9002d936f7

Fix overloaded-virtual warning

src/builders/usbsid-builder/usbsid-emu.cpp
@@ -116,7 +116,7 @@ void USBSID::model(SidConfig::sid_model_t model, MAYBE_UNUSED bool digiboost)
 }
 
 void USBSID::sampling(float systemclock, float freq,
-        SidConfig::sampling_method_t method, bool)
+        SidConfig::sampling_method_t method)
 {
     (void)freq; /* Audio frequency is not used for USBSID-Pico */
     (void)method; /* Interpolation method is not used for USBSID-Pico */

src/builders/usbsid-builder/usbsid-emu.h
@@ -65,7 +65,7 @@ public:
 
 
     void sampling(float systemclock, float freq,
-        SidConfig::sampling_method_t method, bool);
+        SidConfig::sampling_method_t method) override;
 
     void model(SidConfig::sid_model_t model, MAYBE_UNUSED bool digiboost) override;
 

commit f6d816cc049694ecd734976c4b85bd8567c84115

Fix build broken with 178d778

src/builders/usbsid-builder/usbsid-emu.cpp
@@ -116,7 +116,7 @@ void USBSID::model(SidConfig::sid_model_t model, MAYBE_UNUSED bool digiboost)
 }
 
 void USBSID::sampling(float systemclock, float freq,
-        SidConfig::sampling_method_t method)
+        SidConfig::sampling_method_t method, bool)
 {
     (void)freq; /* Audio frequency is not used for USBSID-Pico */
     (void)method; /* Interpolation method is not used for USBSID-Pico */

src/builders/usbsid-builder/usbsid-emu.h
@@ -64,7 +64,7 @@ public:
 
 
     void sampling(float systemclock, float freq,
-        SidConfig::sampling_method_t method) override;
+        SidConfig::sampling_method_t method, bool) override;
 
     void model(SidConfig::sid_model_t model, MAYBE_UNUSED bool digiboost) override;
 