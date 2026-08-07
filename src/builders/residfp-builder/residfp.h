1e3491a39c9581c0ae570f56a65c11bfcdb8cbe5
Leandro Nini <drfiemost@users.sourceforge.net>
2026-08-07 17:52:27 +0200

    Support new residfp tunables

--- src/builders/residfp-builder/residfp.h
+++ src/builders/residfp-builder/residfp.h
@@ -41,7 +41,6 @@ public:
     ReSIDfpBuilder(const char * const name);
     ~ReSIDfpBuilder();
 
-
     const char *getCredits() const;
 
     /// @name global settings
@@ -84,6 +83,34 @@ public:
      */
     void combinedWaveformsStrength(SidConfig::sid_cw_t cws);
     //@}
+
+    /**
+     * Set the DAC leakage level.
+     * Affects the envelope and waveforms.
+     *
+     * @param level the leakage level, between 0 (no leakage) and 1
+     * @since 3.1
+     */
+    void dacLeakage(double level);
+
+    /**
+     * Set the 6581 wave offset.
+     * Affects the volume of digi samples.
+     *
+     * @param offset the waveform offset, between 0 (faint digis) and 1 (loud digis)
+     * @since 3.1
+     */
+    void offset6581(double offset);
+
+    /**
+     * Set the DC-Blocker resistance.
+     * Affects the highpass cutoff frequency.
+     *
+     * @param res the resistance value, between 0 (~1.6Hz) and 1 (~16Hz)
+     * @since 3.1
+     */
+    void dcbRes(double res);
+
 private:
     struct config;
     config *m_config;
